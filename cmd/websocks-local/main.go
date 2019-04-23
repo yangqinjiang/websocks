fe35f00399839fa0f8e442d291bc30c048b3aa64
qin江 <yangqinjiang@qq.com>
2019-04-23 17:19:56 +0800

    d

--- cmd/websocks-local/main.go
+++ cmd/websocks-local/main.go
@@ -1,55 +1,9 @@
 package main
 
 import (
-	"flag"
-	"github.com/yangqinjiang/websocks/core"
-	"net"
-
-	"net/url"
-
-	"github.com/juju/loggo"
+	"fmt"
 )
-
-var localAddr string
-var serverURL string
-var logLevel = loggo.INFO
-var debug bool
-
-var logger = loggo.GetLogger("local")
-
 func main() {
-	flag.StringVar(&serverURL, "u", "ws://localhost:23333/websocks", "server url")
-	flag.StringVar(&localAddr, "l", ":10801", "local listening port")
-	flag.BoolVar(&debug, "debug", false, "debug mode")
-	flag.Parse()
-
-	if debug {
-		logLevel = loggo.DEBUG
-	}
-
-	logger.SetLogLevel(logLevel)
-	logger.Infof("Log level %s", logger.LogLevel().String())
-
-	u, err := url.Parse(serverURL)
-	if err != nil {
-		logger.Errorf(err.Error())
-		return
-	}
-
-	lAddr, err := net.ResolveTCPAddr("tcp", localAddr)
-	if err != nil {
-		logger.Errorf(err.Error())
-	}
-
-	local := core.Local{
-		LogLevel:   logLevel,
-		ListenAddr: lAddr,
-		URL:        u,
-	}
-
-	err = local.Listen()
-	if err != nil {
-		logger.Errorf(err.Error())
-	}
+	fmt.Println("local main....")
 
 }
--- cmd/websocks-server/main.go
+++ cmd/websocks-server/main.go
@@ -1,40 +1,9 @@
 package main
 
 import (
-	"flag"
-
-	"github.com/juju/loggo"
-	"github.com/lzjluzijie/websocks/core"
+	"fmt"
 )
-
-var serverAddr string
-var pattern string
-var debug bool
-var logLevel = loggo.INFO
-
-var logger = loggo.GetLogger("server")
-
 func main() {
-	flag.StringVar(&serverAddr, "l", ":23333", "server listening port")
-	flag.StringVar(&pattern, "p", "/websocks", "server.com/pattern, like password, start with '/'")
-	flag.BoolVar(&debug, "debug", false, "debug mode")
-	flag.Parse()
+	fmt.Println("server main....")
 
-	if debug {
-		logLevel = loggo.DEBUG
-	}
-
-	logger.SetLogLevel(logLevel)
-	logger.Infof("Log level %s", logger.LogLevel().String())
-
-	server := core.Server{
-		LogLevel:   loggo.DEBUG,
-		Pattern:    pattern,
-		ListenAddr: serverAddr,
-	}
-	logger.Infof("Listening at %s", serverAddr)
-	err := server.Listen()
-	if err != nil {
-		logger.Errorf(err.Error())
-	}
 }
